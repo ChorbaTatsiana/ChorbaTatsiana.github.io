fb1867ab19744954f1cb350576a9a68c25b6e5d5
TatianaChorba <moliantv@gmail.com>
2020-04-10 14:31:43 +0300

    my commit for delivery-food

---
+++ Delivery-food/js/js.js
@@ -0,0 +1,13 @@
+let cartButton= document.getElementById("card-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModel);
+
+close.addEventListener("click", toggleModel);
+
+function toggleModel(){
+  modal.classList.toggle("is-open");
+}
+
+new WOW().init();
